7bd8880618449b2492fdd1219ef76e60fcec7152
Raséc Silva <rasec1921@hotmail.com>
2024-07-30 16:08:19 -0300

    Correção de alguns comentários

--- src/createWallet.js
+++ src/createWallet.js
@@ -1,34 +1,34 @@
-//importando as dependencias
+//Importação das dependências
 const bip32 = require('bip32')
 const bip39 = require('bip39')
 const bitcoin = require('bitcoinjs-lib')
 
-//definir a rede
-//bitcoin - rede principal - mainnet
-//testnet - rede de teste - testnet
+// Definição da rede
+// bitcoin: rede principal - mainnet
+// testnet: rede de teste - testnet
 const network = bitcoin.networks.testnet
 
-//derivação de carteiras HD
+// Derivação de carteiras HD
 const path = `m/49'/1'/0'/0` 
 
-//criando o mnemonic para a seed (palavras de senha)
+// Criação do mnemonic para a seed (palavras de senha)
 let mnemonic = bip39.generateMnemonic()
 const seed = bip39.mnemonicToSeedSync(mnemonic)
 
-//criando a raiz da cartiera HD
+// Criação da raiz da carteira HD (Hierarchical Deterministic)
 let root = bip32.fromSeed(seed, network)
 
-//criando uma conta - par pvt-pub keys
+// Criação de uma conta - par pvt-pub keys
 let account = root.derivePath(path)
 let node = account.derive(0).derive(0)
 
-// criando endereço p2phk
+// Criação do endereço p2phk
 let btcAddress = bitcoin.payments.p2pkh({
     pubkey: node.publicKey,
     network: network,
 }).address
 
-// escrevendo dados gerados
+// Escrevendo dados gerados
 console.log("Carteira gerada")
 console.log("Endereço: ", btcAddress)
 console.log("Chave privada:", node.toWIF())
